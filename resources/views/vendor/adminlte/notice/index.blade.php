d08e27352faa0b45b11d20e1eef5e08892be165f
Mazbaul <mazbaul17@gmail.com>
2017-10-10 00:34:57 +0600

    laravel

--- resources/views/vendor/adminlte/notice/index.blade.php
+++ resources/views/vendor/adminlte/notice/index.blade.php
@@ -16,11 +16,11 @@
     </div>
 
     <div class="row">
-      <div class="col-md-10">
+      <div class="col-md-9">
           <h1>ALL NOTICES</h1>
       </div>
-      <div class="col-md-2">
-          <a href="{{route('notice.create')}}" class="btn btn-success btn-lg">Create new</a>
+      <div class="col-md-3">
+          <a href="{{route('notice.create')}}" class="btn btn-primary btn-lg">Create new</a>
       </div>
         <div class="col-md-12">
             <hr>
@@ -43,8 +43,8 @@
                            <td>{!!$notice->tittle!!}</td>
                            <td>{!!substr($notice->notice,0,50)!!}{!!strlen($notice->notice)>50 ?"..........." :""!!}</td>
                            <td>{{date('M j, Y',strtotime($notice->created_at))}}</td>
-                           <td><a href="{{route('notice.show',$notice->id)}}" class="btn btn-sm btn-default">VIEW</a> </td>
-                           <td><a href="{{route('sendemail',$notice->id)}}" class="btn btn-sm btn-default">SEND TO DEPARTMENT</a> </td>
+                           <td><a href="{{route('notice.show',$notice->id)}}" class="btn btn-sm btn-success">VIEW</a> </td>
+                           <td><a href="{{route('sendemail',$notice->id)}}" class="btn btn-sm btn-primary">SEND TO DEPARTMENT</a> </td>
                        </tr>
                    @endforeach
 
--- resources/views/vendor/adminlte/user/index.blade.php
+++ resources/views/vendor/adminlte/user/index.blade.php
@@ -10,10 +10,10 @@
             <h1>ALL STUDENTS</h1>
         </div>
         <div class="col-md-4">
-            <a href="{{route('departments.create')}}" class="btn btn-success btn-lg">Add New Department</a>
+            <a href="{{route('departments.create')}}" class="btn btn-primary btn-lg">Add New Department</a>
         </div>
         <div class="col-md-2">
-            <a href="{{route('users.create')}}" class="btn btn-success btn-lg">Add New Student</a>
+            <a href="{{route('users.create')}}" class="btn btn-primary btn-lg">Add New Student</a>
         </div>
         <div class="col-md-12">
             <hr>
@@ -35,7 +35,7 @@
                         <th>{{$department->name}}</th>
                         <th>{{$department->users()->count()}}</th>
 
-                        <td><a href="{{route('users.show',$department->id)}}" class="btn btn-sm btn-default">VIEW ALL STUDENT</a> </td>
+                        <td><a href="{{route('users.show',$department->id)}}" class="btn btn-sm btn-success">VIEW ALL STUDENT</a> </td>
                     </tr>
                 @endforeach
 
