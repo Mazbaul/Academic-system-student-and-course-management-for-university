14e6e76b95e51ba08a8d6d02fdd9beb395b4dc3b
Mazbaul <mazbaul17@gmail.com>
2017-10-11 02:00:04 +0600

    laravel

--- resources/views/vendor/adminlte/user_home.blade.php
+++ resources/views/vendor/adminlte/user_home.blade.php
@@ -8,8 +8,23 @@
 @section('main-content')
 
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-  
+
+    <div class="col-md-8">
+      <h2><strong>Name : </strong>{{Auth::user()->name}}</h2>
+      <h4><strong>Department Name : </strong>{{Auth::user()->department->name}}</h4>
+      <h4><strong>Session : </strong>{{Auth::user()->academicssn}}</h4>
+      <h4><strong>Student ID : </strong>{{Auth::user()->studentid}}</h4>
+      <h4><strong>Email : </strong>{{Auth::user()->email}}</h4>
+      <h4><strong>Phone Number : </strong>{{Auth::user()->mobile_number}}</h4>
+    </div>
+</div>
+<div class="row">
+  <div class="col-md-6">
+    <h2>Registered Courses</h2>
+  </div>
+  <div class="col-md-6">
+    <h2>Registered Backlog courses</h2>
+  </div>
 </div>
 
 @endsection
