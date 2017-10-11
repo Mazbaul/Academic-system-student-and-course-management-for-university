79582bf6fe847317cd6f6f7fff5e10223d8b3529
Mazbaul <mazbaul17@gmail.com>
2017-10-12 01:15:11 +0600

    laravel

--- resources/views/vendor/adminlte/pdf/BacklogCourseregform.blade.php
+++ resources/views/vendor/adminlte/pdf/BacklogCourseregform.blade.php
@@ -23,34 +23,34 @@
 
 <body>
   <div class="container">
-<div class="row">
-  <h2 style="text-align:center;">Noakhali Science And Technology University</h2>
-  <h4 style="text-align:center;">Noakhali-3814,Bangladesh</h4><br>
-    <h3 style="text-align:center;">Backlog Course Registration Form</h3><br><br>
-    <div class="col-md-2" style=" padding-left:600px;padding-bottom:-120px;">
-      <div style="height:100px; border:1px solid; text-align:center; padding-top:50px;">Add Photo Here</div>
+    <div class="row">
+      <h2 style="text-align:center;">Noakhali Science And Technology University</h2>
+      <h4 style="text-align:center;">Noakhali-3814,Bangladesh</h4>
+        <h3 style="text-align:center;">Course Registration Form</h3>
+        <div class="col-md-2" style=" padding-left:620px;padding-top:-150px;">
+          <div style="height:120px; border:1px solid; text-align:center; padding-top:20px;">Add Photo Here</div>
+        </div>
     </div>
-</div>
 
 
     <div class="row">
       <div class="col-md-8 " style="padding-left:50px;">
-<div class="col-md-2"><strong>Student Name </strong>:{{Auth::User()->name}}</div><br>
-<div class="col-md-2"><strong>Roll </strong>:{{Auth::User()->studentid}}</div><br>
-<div class="col-md-2"><strong>Email </strong>:{{Auth::User()->email}}</div><br>
-<div class="col-md-2"><strong>Session </strong>:{{Auth::User()->academicssn}}</div><br>
-<div class="col-md-2"><strong>Father's Name:</strong>............................</div><br>
-<div class="col-md-2"><strong>Mother's Name:</strong>............................</div><br>
-<div class="col-md-2"><strong>Department Name </strong>:{{Auth::User()->department->name}}</div><br>
-<div class="col-md-2"><strong>Hall :</strong>Abdus Salam Hall</div><br>
-<div class="col-md-2"><strong>Date Of Commencement Of Examination:</strong>............................</div><br><br>
+        <div class="col-md-2"><strong>Student Name </strong>:{{Auth::User()->name}}</div><br>
+        <div class="col-md-2" style="text-align:left;padding-top:-5px;padding-right:50px;"><strong>Father's Name:</strong>............................</div><br>
+        <div class="col-md-2" style="text-align:right;padding-top:-35px;padding-right:80px;"><strong>Mother's Name:</strong>............................</div>
+        <div class="col-md-2" style="text-align:left;"><strong>Roll </strong>:{{Auth::User()->studentid}}</div>
+        <div class="col-md-2" style="text-align:center;padding-top:-20px;padding-right:50px;"><strong>Email </strong>:{{Auth::User()->email}}</div>
+        <div class="col-md-2" style="text-align:right;padding-top:-20px;padding-right:80px;"><strong>Session </strong>:{{Auth::User()->academicssn}}</div><br>
+        <div class="col-md-2" style="text-align:left;padding-top:-5px;padding-right:50px;"><strong>Department Name </strong>:{{Auth::User()->department->name}}</div>
+        <div class="col-md-2" style="text-align:right;padding-top:-20px;padding-right:80px;"><strong>Hall :</strong>Abdus Salam Hall</div><br>
+        <div class="col-md-2"><strong>Date Of Commencement Of Examination:</strong>............................</div><br><br>
 </div>
 </div>
 <div style="padding-left:50px;">
-<div style="padding-left:90px;">
+<div style="padding-left:50px;width:100%">
   <table>
       <tr>
-          <th>Sl No.</th>
+
           <th>Course Code    </th>
           <th>   Course Title</th>
 
@@ -63,7 +63,7 @@
 
 
           <tr>
-              <td></td>
+
               <td>{{$backlog->course_code}}</td>
               <td>{{$backlog->course_name}}</td>
               <td>{{$backlog->course_year}}</td>
@@ -106,21 +106,21 @@
 <div class="admitcard">
 
   <h2 style="text-align:center;">Noakhali Science And Technology University</h2>
-  <h4 style="text-align:center;">Noakhali-3814,Bangladesh</h4><br>
-  <h3 style="text-align:center;">Admit Card</h3><br>
-    <div class="col-md-2" style=" padding-left:600px;padding-bottom:-120px;">
-      <div style="height:50px; border:1px solid; text-align:center; padding-top:50px;">Add Photo Here</div>
-    </div>
+  <h4 style="text-align:center;">Noakhali-3814,Bangladesh</h4>
+  <h3 style="text-align:center;">Admit Card</h3>
+  <div class="col-md-2" style=" padding-left:620px;padding-top:-150px;">
+    <div style="height:120px; border:1px solid; text-align:center; padding-top:20px;">Add Photo Here</div>
+  </div>
     <div class="col-md-8 " style="padding-left:50px;">
       <div style="width:200px;">
 <div class="col-md-2" style="left:50px;"><strong>Student Name </strong>:{{Auth::User()->name}}</div><br>
 <div class="col-md-2" style="left:60px;"><strong>Roll </strong>:{{Auth::User()->studentid}}</div><br>
 </div>
 <div class="col-md-2"><strong>Session </strong>:{{Auth::User()->academicssn}}</div><br>
-<div style="padding-left:90px;">
+<div style="padding-left:50px;width:100%;">
   <table>
       <tr>
-          <th>Sl No.</th>
+
           <th>Course Code    </th>
           <th>   Course Title</th>
 
@@ -133,7 +133,7 @@
 
 
           <tr>
-              <td></td>
+
               <td>{{$backlog->course_code}}</td>
               <td>{{$backlog->course_name}}</td>
               <td>{{$backlog->course_year}}</td>
